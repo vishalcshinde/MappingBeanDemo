6590497cf6ed9e91e6326538006fddea9d8d3e50
Vishal Shinde <vish.c.shinde@gmail.com>
2015-04-02 01:43:34 +0530

    changed the variable name

--- src/main/java/com/vishal/MyController.java
+++ src/main/java/com/vishal/MyController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 	
-	@RequestMapping("/hello/{name}")
-	public String sayHello(@PathVariable List<PropertyVo> name) {
-		return "hello " + name;
+	@RequestMapping(value = "/hello/{properties}")
+	public String sayHello(@PathVariable List<PropertyVo> properties) {
+		return properties.toString();
 	}
 }
